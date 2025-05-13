946afd8d79c087659847a8ae6aac25302484bb41
chait <chait@VARDHAN>
2025-05-13 00:20:04 -0500

    developed newcode

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
+})
+export class AppComponent implements OnInit {
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+}
